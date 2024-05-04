e01694c13aa35d53f3b1d717db3a59f55c846c86
lamzaone <denisdenis009@gmail.com>
2024-05-04 21:16:09 +0300

    updater stuff to test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from utils import serversettings as Settings, queues as Queues, update
 import os
 from config import config  # Make sure this import points to your bot's configuration
@@ -25,12 +25,33 @@ async def load_cogs():
             print(f"[-] An error occurred while loading {cog}: {e}")
 
 
-
+@tasks.loop(seconds=20)
+async def check_for_updates():
+    updates = update.check_upd(is_windows)
+    if updates:
+        app_info = await bot.application_info()
+        owner = app_info.owner
+        embed = discord.Embed(title="Updates are available", description=updates, color=discord.Color.green())
+        message = await owner.send(embed=embed)
+        #add reaction to the message to allow the owner to update the bot
+        await message.add_reaction('✅')
+        await message.add_reaction('❌')            
+        
+        try:
+            reaction, _ = await bot.wait_for('reaction_add', timeout=12*3600.0, check=lambda reaction, user: user == owner and reaction.message == message)
+            if reaction.emoji == '✅':
+                await owner.send('[+] Update accepted. Updating bot...')
+                update.update(is_windows)                    
+                sys.exit(0)
+            elif reaction.emoji == '❌':
+                await owner.send('[-] Update declined. Bot will not be updated.')
+        except asyncio.TimeoutError:
+            await owner.send('[-] Update declined. Bot will not be updated.')
+        
 
 @bot.event
 async def on_ready():
     await load_cogs()
-    await update.check_for_updates(bot, is_windows)
     print(f'[+] Booted {bot.user}...')
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="/play <song>"), status=discord.Status.dnd)
     # Reset queues and fetch settings for all guilds
@@ -75,6 +96,8 @@ async def on_ready():
     # Print URL for inviting the bot to a server
     oauth_url = discord.utils.oauth_url(bot.application_id, permissions=discord.Permissions(permissions=8))
     print(f'[+] Invite URL: {oauth_url}')
+    
+    await check_for_updates.start()
 
 
 ### INITIALIZE GUILD SETTINGS AND QUEUES ON GUILD JOIN ###
