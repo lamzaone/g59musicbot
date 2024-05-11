ba665a5f9aa72e14d18d199992cff55ecbc82c6b
lamzaone <denisdenis009@gmail.com>
2024-05-11 22:46:15 +0300

    #

--- cogs/videoplayer.py
+++ cogs/videoplayer.py
@@ -8,6 +8,8 @@ import yt_dlp
 import asyncio
 import os
 from requests import get
+import requests
+import json
 
 ip = get('https://api.ipify.org').content.decode('utf8')
 
@@ -17,8 +19,8 @@ class VideoPlayer(commands.Cog):
 
     @commands.command()
     async def playvideo(self, ctx, url):
-        if os.path.exists('video_streaming/static/video.webm'):
-            os.remove('video_streaming/static/video.webm')            
+        if os.path.exists('video_streaming/static/video.mp4'):
+            os.remove('video_streaming/static/video.mp4')            
 
         for proc in psutil.process_iter():
             try:
@@ -32,11 +34,26 @@ class VideoPlayer(commands.Cog):
             'format': 'bestvideo+bestaudio/best',
             'outtmpl': 'video_streaming/static/video.webm',
             'default_search': 'auto',
+            'mp4': True,
         }
 
         async with ctx.typing():
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
+            try:
+                # code start here
+                idname= "overlord-iv"
+                url = f"http://127.0.0.1:3000/anime/gogoanime/watch/spy-x-family-episode-1"
+                response = requests.get(url)
+                print(response)
+                data = response.json()
+                link = data['sources'][::-1][1]['url']
+                filename2= f"video_streaming/static/video.mp4"
+                f = f'ffmpeg -i {link} -c copy {filename2}'
+                # execute the command
+                with os.popen(f):
+                    pass
+            except Exception as e:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([url])
 
         print(ip)
         # port = int(str(ctx.guild.id)[-4:])
