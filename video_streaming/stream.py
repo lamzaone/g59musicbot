d41037d4cfa929030f60c1621ef7af605790d1ab
lamzaone <denisdenis009@gmail.com>
2024-05-15 03:47:25 +0300

    #

--- video_streaming/stream.py
+++ video_streaming/stream.py
@@ -1,8 +1,6 @@
-from flask import Flask, make_response, request, current_app
-from datetime import timedelta
-from functools import update_wrapper
+from flask import Flask, Response, render_template, request
 from flask_socketio import SocketIO
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS, cross_origin  # Import CORS
 from flask_cors import CORS
 from flask import jsonify
 
@@ -11,53 +9,17 @@ import sys
 import requests
 
 app = Flask(__name__)
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
+socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-    def get_methods():
-        if methods is not None:
-            return methods
+cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
 
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
-@app.route('/foo', methods=['GET','POST','OPTIONS'])
-@crossdomain(origin="*")
+@app.route('/foo', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def foo():
     return request.json['inputVar']
-socketio = SocketIO(app)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 url = ""
