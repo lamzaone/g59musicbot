2d2c5267b302d4c5536586f3fb2ca6f994ba7ea3
Denis <denisdenis009@gmail.com>
2025-03-11 21:53:29 +0200

    #

--- cogs/cookies.py
+++ cogs/cookies.py
@@ -1,7 +1,6 @@
-import discord
+import asyncio
 from discord.ext import commands
 from config import config
-import os
 
 class Cookies(commands.Cog):
     def __init__(self, bot):
@@ -10,25 +9,27 @@ class Cookies(commands.Cog):
     @commands.hybrid_command(name='cookies',  brief='Add youtube cookies', description='Add youtube cookies for bypassing youtube authentication')
     @commands.is_owner()
     async def cookies(self, ctx):
-        # get message content without !cookies command
-        message = ctx.message.content[len(ctx.prefix)+len(ctx.invoked_with):].strip()
-        # check if the message is empty
-        if not message:
-            await ctx.send("No cookies provided")
-            return
-        # check if the message has an attachment (cookie file)
-        if ctx.message.attachments:
-            # get the first attachment
-            attachment = ctx.message.attachments[0]
-            # check if the attachment is a text file
-            if attachment.filename.endswith('.txt'):
-                # download the attachment
-                await attachment.save(config.cookies_file)
-                await ctx.send("Cookies saved")
-                return
-        with open(config.cookies_file, 'w') as f:
-            f.write(message)
-        await ctx.send("Cookies saved")
+        # after cookies command used, ask for a new message from the same user with the cookies
+        await ctx.send("Please send the cookies in the next message")
+        def check(m):
+            return m.author == ctx.author
+        try:
+            message = await self.bot.wait_for('message', check=check, timeout=60)
+            # check if cookies were sent as an attachment, or as text
+            if message.attachments:
+                cookies = await message.attachments[0].read()
+                with open(config.cookies_file, 'ab') as f:  # append in binary mode
+                    f.write(cookies)
+            else:
+                cookies = message.content
+                # append the cookies to the cookies file
+                with open(config.cookies_file, 'a') as f:  # append in text mode
+                    f.write(cookies)
+            await ctx.send("Cookies have been added.")
+        except asyncio.TimeoutError:
+            await ctx.send("[-] Timed out. Please try again.")
+        except Exception as e:
+            await ctx.send(f"[-] An error occurred: {e}")
 
 async def setup(bot):
     await bot.add_cog(Cookies(bot))
